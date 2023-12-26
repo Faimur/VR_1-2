3d324a9010586da2c9fa9be84ed49c0536286bd8
Faimur <127348100+Faimur@users.noreply.github.com>
2023-12-26 06:43:07 +0300

    Add files via upload

---
+++ Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Text x;
+    public int c;
+    public GameObject Testobj;
+    bool a = true;
+
+    public void Deactive()
+    { 
+        if (a == true){
+            Testobj.SetActive(false);
+            a = false;
+        }
+        else{
+            Testobj.SetActive(true);
+            a = true;
+    }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        c-=1;
+        x.text = c.ToString();
+    
+    }
+}
